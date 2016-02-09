87271e382b9da129f841c9df859230e13797e1c3
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-09 14:22:34 +0100

    adds first algorithm to calculate spare bonus

--- BowlingKata/Bowling.swift
+++ BowlingKata/Bowling.swift
@@ -1,18 +1,28 @@
 class Bowling {
     
     var score:Int {
-        return rolls.reduce(0) { $0 + $1.score }
+        var previousSpare = false
+        var score = 0
+        for frame in frames {
+            if previousSpare {
+                score += frame.firstRoll
+                previousSpare = false
+            }
+            score += frame.score
+            previousSpare = (frame.score == 10)
+        }
+        return score
     }
     
     private var currentRoll = 0
-    private var rolls = [Frame](count: 10, repeatedValue: Frame())
+    private var frames = [Frame](count: 10, repeatedValue: Frame())
     
     func roll(pins:Int){
         if currentRoll % 2 == 0 {
-            rolls[currentRoll/2].firstRoll = pins
+            frames[currentRoll/2].firstRoll = pins
         }
         else {
-            rolls[currentRoll/2].secondRoll = pins
+            frames[currentRoll/2].secondRoll = pins
         }
         currentRoll += 1
     }
