b4fe4e22fb77ab01716d9be61f686d6f66d53b99
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-10 14:15:08 +0100

    removes obsolete knockedPins var

--- BowlingKata/Bowling.swift
+++ BowlingKata/Bowling.swift
@@ -7,12 +7,10 @@ class Bowling {
         return score
     }
     
-    private var knockedPins = 0
     private var frames = [Frame](count: 10, repeatedValue: Frame())
     private var currentRoll = 0
     
     func roll(pins:Int){
-        knockedPins += pins
         if currentRoll % 2 == 0 {
             frames[currentRoll/2].firstRoll = pins
         }
