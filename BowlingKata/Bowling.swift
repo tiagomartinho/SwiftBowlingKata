cf763e6294b1c0f3fdc1af20b3b3aaa6489b31de
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-10 14:13:40 +0100

    register knockedPins in frames array

--- BowlingKata/Bowling.swift
+++ BowlingKata/Bowling.swift
@@ -9,6 +9,12 @@ class Bowling {
     
     func roll(pins:Int){
         knockedPins += pins
+        if currentRoll % 2 == 0 {
+            frames[currentRoll/2].firstRoll = pins
+        }
+        else {
+            frames[currentRoll/2].secondRoll = pins
+        }
         currentRoll += 1
     }
 }
