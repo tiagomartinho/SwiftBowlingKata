8e99cdf9761fa2cf31a41e335a30dddf79bd6161
tiagomartinho <tiago.martinho@xpeppers.com>
2016-02-10 14:12:24 +0100

    defines current roll var

--- BowlingKata/Bowling.swift
+++ BowlingKata/Bowling.swift
@@ -1,13 +1,14 @@
 class Bowling {
-    
-    private var knockedPins = 0
-    private var frames = [Frame](count: 10, repeatedValue: Frame())
-    
     var score:Int{
         return knockedPins
     }
     
+    private var knockedPins = 0
+    private var frames = [Frame](count: 10, repeatedValue: Frame())
+    private var currentRoll = 0
+    
     func roll(pins:Int){
         knockedPins += pins
+        currentRoll += 1
     }
 }
